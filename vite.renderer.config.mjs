45c59ed6a04ef0bd555bda183bbe88cf6c681450
John Crimmins <johncrimmins@Mac.verizon.net>
2025-10-28 17:29:15 -0400

    Import screen working

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: './',
   build: {
-    rollupOptions: {
-      input: 'src/renderer.js'
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
 });
